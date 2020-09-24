262e8a44d946da32e29b41dc040e88eb9f35de80
Raúl Morales <raulmoralesruiz@outlook.com>
2020-09-24 20:20:54 +0200

    se incluye ejercicio 4 'galeria JS' en JS/ej_basicos

--- JS/ej_basicos/4_galeria/scripts.js
+++ JS/ej_basicos/4_galeria/scripts.js
@@ -1,33 +1,51 @@
 //Dada la siguiente galería, como harías para añadir un evento a cada uno de los div y saber en cual se ha hecho click
 const galeria = document.getElementById('gallery');
 
-galeria.addEventListener("click", (e) => {
-    /* console.log(e.target.textContent); */
-    /* console.log(e.target); */
-    let numDiv = e.target.textContent;
-    let it = document.getElementsByClassName('gallery__item')[numDiv - 1];
-
-    let claseActual = it.classList.value;
-    console.log(claseActual);
-
-    if (e.target.className.match("gallery__item")) {
-        e.target.className.replace('gallery__item', 'gallery__itemYellow');
+// FORMA 1 - funciona
+galeria.addEventListener('click', (e) => {
+    //se guarda la lista de clases
+    let clase = e.target.classList;
+    
+    //se guarda el número del div seleccionado y se muestra por consola (solo celdas)
+    let div = e.target.textContent;
+    if (div.length === 1) {
+        console.log("Div seleccionado: " + div);
     }
 
-    /* if (claseActual == "gallery__item") {
-        it.classList.replace('gallery__item', 'gallery__itemYellow');
+    //si la lista de clases contiene es gallery_item
+    if (clase.contains('gallery_item')) {
+        //se elimina gallery_item
+        clase.remove('gallery_item');
+
+        //se añade gallery_item_yellow
+        clase.add('gallery_item_yellow');
+        
+    //si la clase es gallery_item_yellow
     } else {
-        it.classList.replace('gallery__itemYellow', 'gallery__item');
-    } */
+        //se elimina gallery_item_yellow
+        clase.remove('gallery_item_yellow');
 
-
-    //className
-    //match
-
-
-    /* console.log(it); */
-    /* console.log(it.classList.value) */
-
-
-    /* it.classList.replace('gallery__item', 'gallery__itemYellow'); */
+        //se añade gallery_item
+        clase.add('gallery_item');
+    }
 });
+
+
+
+// FORMA 2 - no funciona
+// galeria.addEventListener('click', (e) => {
+
+//     console.log("antes: " + e.target.className);
+//     console.log("antes: " + e.target.classList);
+
+//     //e.preventDefault();
+//     if (e.target.className.match("gallery_item")) {
+//         e.target.classList.replace('gallery_item', 'gallery_item_yellow');
+//     }
+//     else{
+//         e.target.classList.replace('gallery_item_yellow', 'gallery_item');
+//     }
+
+//     console.log("despu: " + e.target.className);
+//     console.log("despu: " + e.target.classList);
+// })
